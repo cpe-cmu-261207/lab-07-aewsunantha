36bf76d576547bea28b985f35dade4538c3e6efa
aewsunantha <59277896+aewsunantha@users.noreply.github.com>
2021-02-02 10:21:01 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import {useEffect} from 'react';
-import axios from 'axios'
-const baseURL = 'https://dummyapi.io/data/api'
+import { useEffect } from "react";
+import axios from "axios";
+const baseURL = "https://dummyapi.io/data/api";
 const Home = () => {
-  return (
-      <h1>This is home page</h1>
-  )
-}
+  return <h1>This is home page Hi!!!!</h1>;
+};
 
-export default Home
+export default Home;
