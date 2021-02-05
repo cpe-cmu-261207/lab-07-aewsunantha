b14e189ea0afa2a5726ee37ae641bf02e287fb21
Sunantha Rueangrit <sunantha1881@gmail.com>
2021-02-05 20:27:53 +0700

    sent

---
+++ pages/gallery.js
@@ -0,0 +1,69 @@
+import Link from "next/link";
+const Gallery=()=>{
+    return(
+        <div id="contact">
+            <table style={{width:"100%",textAlign: "center",backgroundColor:"#ff9f80",borderStyle:"double",borderWidth: "10px",borderColor:"#fcf7e9"}}>
+            <tr>
+                <td> <Link href={"/home"}><p style={{textAlign:"center"}}> Home </p></Link> </td>
+                <td> <Link href={"/gallery"}><p style={{textAlign:"center"}}> Gallery </p></Link> </td>
+                <td> <Link href={"/calgpa"}><p style={{textAlign:"center"}}> GPA Calculator </p></Link> </td>
+                <td> <Link href={"/contact"}><p style={{textAlign:"center"}}> Contact </p></Link> </td>
+                <td> <Link href={"/post"}><p style={{textAlign:"center"}}> Post </p></Link> </td>
+            </tr> 
+            </table>
+
+            <table width={{"80%":align="center", style="padding: 15px;, border:1px solid;"}}>
+              <tr style={{textAlign: "center"}}>
+                  <td><h1>Sunantha  Rueangrit</h1>
+                  <h2>Web Developer</h2>
+                  <h3><Link href="mailto:sunantha_ru@cmu.ac.th">sunantha_ru@cmu.ac.th</Link></h3>
+                  <h3>0812345688</h3></td>
+                  <td><img src="pic1.jpg" alt="myself" style={{width:"250px",height:"250px",alignItems: "baseline"}}></img></td>
+              </tr>
+              </table>
+
+             
+        <h2   style="background-color:rgb(176, 159, 236);" align="center">Gallery</h2>
+        
+        <table align="center" style="width:80%" style="padding: 15px;  border:1px solid;">
+          <tr align="center">
+            <td><img src="Cake.jpg" style={{width="250%" ,alt="This is Cake"}}></img></td>
+            <td><img src="Pancakes.jpg" style={{width="250%" ,alt="This is Pancakes"}}></img></td> 
+            <td><img src="Macaroons.jpg" style={{width="250%" ,alt="This is Macaroons"}}></img></td>
+            <td><img src="cubcake.jpg"style={{width="250%" ,alt="This is cubcake"}}></img></td>
+          </tr>
+          <tr align="center">
+            <td>Cake</td>
+            <td>Pancakes</td>
+            <td>Macaroons</td>
+            <td>cubcake</td>
+          </tr>
+          <tr align="center">
+            <td><img src="Noodles.jpg" style={{width="250%" ,alt="This is Noodles"}}></img></td>
+            <td><img src="Bibimbap.jpg"style={{width="250%"}}></img></td>
+            <td><img src="chicken.jpg"style={{width="250%"}}></img></td>
+            <td><img src="Sushi.jpg" style={{width="250%"}}></img></td>
+          </tr>
+          <tr align="center">
+            <td>Noodles</td>
+            <td>Bibimbap</td>
+            <td>chicken</td>
+            <td>Sushi</td>
+          </tr>
+          <tr align="center">
+            <td><img src="Cantaloupe.jpg" style={{width="250%"}}></img></td>
+            <td><img src="watermelon.jpg" style={{width="250%"}}></img></td>
+            <td><img src="Passion.jpg"style={{width="250%"}}></img></td>
+            <td><img src="Coco.jpg"style={{width="250%"}}></img></td>
+          </tr>
+          <tr align="center">
+            <td>Cantaloupe</td>
+            <td>watermelon</td>
+            <td>Passion</td>
+            <td>Coco</td>
+          </tr>
+        </table>
+        </div>
+    )
+}
+export default Gallery;
