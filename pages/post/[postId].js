6f1adf3df3b9948ab4dccd97f9eead59f73faf5a
aewsunantha <59277896+aewsunantha@users.noreply.github.com>
2021-02-09 17:30:15 +0700

    sent

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,19 +1,67 @@
-import {useRouter} from "next/router";
-import {useEffect,useState} from 'react'
-import axios from 'axios'
-const baseURL = 'https://dummyapi.io/data/api'
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+const baseURL = "https://dummyapi.io/data/api";
+const password = "6015521ee7dfad6b195ef4d4";
 const Post = () => {
-    const router = useRouter()
-    const {postId} = router.query
-    const [post,setPost] = useState(null)
-    const [comment,setComment] = useState([])
+  const router = useRouter();
+  const { postId } = router.query;
+  const [post, setPost] = useState(null);
+  const [comment, setComment] = useState([]);
 
-    return (
-        <>
-            <h1>display post data from api here</h1>
-        </>
-    )
-}
+  useEffect(() => {
+    if (!postId) {
+      return;
+    }
+    axios
+      .get(baseURL + "/post/" + postId, { headers: { "app-id": password } })
+      .then((respond) => {
+        console.log(respond);
+        setPost(respond.data);
+      });
+    axios
+      .get(baseURL + "/post/" + postId + "/comment", {
+        headers: { "app-id": password }
+      })
+      .then((respond) => {
+        console.log(respond);
+        setComment(respond.data.data);
+      });
+  }, [postId]);
 
-export  default  Post
+  return (
+    <>
+      {post && (
+        <div>
+          <h1>{"Post : " + post.text}</h1>
+          <p>{"tags : " + post.tags}</p>
+          <p>
+            <p style={{ textAlign: "center" }}>
+              <img className="imgflame" src={post.image}></img>
+            </p>
+            <p>{post.owner.firstName + " " + post.owner.lastName}</p>
+            <p>{"Likes : " + post.likes}</p>
+            {comment.map((item) => {
+              return (
+                <div key={item.id}>
+                  <p>
+                    {item.owner.firstName + " " + item.owner.lastName + " : "}
+                    {item.message}
+                  </p>
+                </div>
+              );
+            })}
+          </p>
+        </div>
+      )}
+      <div>
+        <Link href={"/post"}>
+          <button>Go back</button>
+        </Link>
+      </div>
+    </>
+  );
+};
 
+export default Post;
--- pages/post/index.js
+++ pages/post/index.js
@@ -1,16 +1,83 @@
-import {useEffect,useState} from 'react'
-import Link from 'next/link'
-import axios from 'axios'
-const baseURL = 'https://dummyapi.io/data/api'
-
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import axios from "axios";
+const baseURL = "https://dummyapi.io/data/api";
+const password = "6018d9989e74d88d234d0aee";
 const Posts = () => {
-    const [posts,setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get(baseURL + "/post", { headers: { "app-id": password } })
+      .then((respond) => {
+        console.log(respond);
+        setPosts(respond.data.data);
+      });
+  }, []);
+  return (
+    <>
+      <table
+        style={{
+          width: " 100%",
+          backgroundColor: "#ff9f80",
+          borderStyle: "double",
+          borderWidth: "10px",
+          borderColor: "#fcf7e9"
+        }}
+      >
+        <tr align="center">
+          <td>
+            {" "}
+            <Link href={"/home"}>
+              <p style={{ textAlign: "center" }}> Home </p>
+            </Link>{" "}
+          </td>
+          <td>
+            {" "}
+            <Link href={"/gallery"}>
+              <p style={{ textAlign: "center" }}> Gallery </p>
+            </Link>{" "}
+          </td>
+          <td>
+            {" "}
+            <Link href={"/calgpa"}>
+              <p style={{ textAlign: "center" }}> GPA Calculator </p>
+            </Link>{" "}
+          </td>
+          <td>
+            {" "}
+            <Link href={"/contact"}>
+              <p style={{ textAlign: "center" }}> Contact </p>
+            </Link>{" "}
+          </td>
+          <td>
+            {" "}
+            <Link href={"/post"}>
+              <p style={{ textAlign: "center" }}> Post </p>
+            </Link>{" "}
+          </td>
+        </tr>
+      </table>
+      <h2>All Posts</h2>
 
-    return (
-        <>
-            <h1>All Posts</h1>
-        </>
-    )
-}
+      {posts.map((item) => {
+        return (
+          <div key={item.id}>
+            <h4>{"Post : " + item.text}</h4>
+            <p style={{ textAlign: "center" }}>
+              <Link href={"/post/" + item.id}>
+                <img
+                  className="imgflame"
+                  src={item.image}
+                  style={{ width: "70%" }}
+                ></img>
+              </Link>
+              <p style={{ color: "#ff9f80" }}>{"Likes : " + item.likes}</p>
+            </p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-export default  Posts
+export default Posts;
